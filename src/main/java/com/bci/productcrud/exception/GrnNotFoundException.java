97961b35a33ced55fb1c142445e747409ec11d51
nethmihimashi108@gmail.com <nethmihimashi108@gmail.com>
2026-08-06 07:30:51 +0530

    Grn not found exception

---
+++ src/main/java/com/bci/productcrud/exception/GrnNotFoundException.java
@@ -0,0 +1,8 @@
+package com.bci.productcrud.exception;
+
+public class GrnNotFoundException extends RuntimeException {
+
+    public GrnNotFoundException(String message) {
+        super(message);
+    }
+}
