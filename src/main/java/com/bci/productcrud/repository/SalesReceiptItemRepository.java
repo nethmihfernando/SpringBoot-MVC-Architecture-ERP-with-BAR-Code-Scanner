5d3c1f4c814add25a3b8003d1c753f0821b92470
nethmihimashi108@gmail.com <nethmihimashi108@gmail.com>
2026-08-16 07:12:15 +0530

    Sales receipt

---
+++ src/main/java/com/bci/productcrud/repository/SalesReceiptItemRepository.java
@@ -0,0 +1,4 @@
+package com.bci.productcrud.repository;
+import com.bci.productcrud.model.SalesReceiptItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface SalesReceiptItemRepository extends JpaRepository<SalesReceiptItem, Long> {}
