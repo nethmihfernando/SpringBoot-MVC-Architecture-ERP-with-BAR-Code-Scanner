466a2bc4620491e39d149114011549305e009838
nethmihimashi108@gmail.com <nethmihimashi108@gmail.com>
2026-08-16 07:07:44 +0530

    Update purchase order

--- src/main/java/com/bci/productcrud/repository/PurchaseOrderRepository.java
+++ src/main/java/com/bci/productcrud/repository/PurchaseOrderRepository.java
@@ -1,13 +1,7 @@
 package com.bci.productcrud.repository;
-
 import com.bci.productcrud.model.PurchaseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
+import java.util.List;
 public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long> {
-
-    Optional<PurchaseOrder> findByPoNumber(String poNumber);
-
-    boolean existsByPoNumber(String poNumber);
+    List<PurchaseOrder> findBySupplierId(Long supplierId);
 }
