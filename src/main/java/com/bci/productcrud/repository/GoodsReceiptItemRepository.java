84571a51113a7bef1aac97758727ca94d0849bea
nethmihimashi108@gmail.com <nethmihimashi108@gmail.com>
2026-08-16 06:51:57 +0530

    Goods receipt

---
+++ src/main/java/com/bci/productcrud/repository/GoodsReceiptItemRepository.java
@@ -0,0 +1,4 @@
+package com.bci.productcrud.repository;
+import com.bci.productcrud.model.GoodsReceiptItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface GoodsReceiptItemRepository extends JpaRepository<GoodsReceiptItem, Long> {}
